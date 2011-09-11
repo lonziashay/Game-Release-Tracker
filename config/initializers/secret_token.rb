a58904623afcc95b78627ab2eb4815eeb8d84197
Lonzia Shay <lshay@YPCMC09233.(none)>
2011-09-10 20:12:28 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Grtracker::Application.config.secret_token = 'd575367e3a6d6861a45efa4d69689398792e3192ae4a3f46824726d4ac519dd2d0b121fffddca2430c1631e7f42d5d76cbaf5a16625cea86920358769a23df24'
